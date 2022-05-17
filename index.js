f46f3b0376ba0955f0cfa3889d657f4984eccae6
Teddy Milford <teddy.milford@gmail.com>
2022-05-17 09:37:57 -0600

    commit tree fc13bda690808ab1e535c1ee5a0123b3dd491295

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = function (name) {
+  cats.push(name);
+};
+
+const destructivelyPrependCat = function (name) {
+  cats.unshift(name);
+};
+
+const destructivelyRemoveLastCat = function (name) {
+  cats.pop();
+};
+const destructivelyRemoveFirstCat = function (name) {
+  cats.shift();
+};
+
+const appendCat = function (name) {
+  let newCats = [...cats, name];
+  return newCats;
+};
+const prependCat = function (name) {
+  let newCats = [name, ...cats];
+  return newCats;
+};
+
+const removeLastCat = function () {
+  let newCats = cats.slice(0, cats.length - 1);
+  return newCats;
+};
+const removeFirstCat = function () {
+  let newCats = cats.slice(1);
+  return newCats;
+};
